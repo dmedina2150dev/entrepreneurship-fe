aaf78b69503788561605e844e02d7c233183333d
dmedina2150dev <dmedina2150dev@gmail.com>
2022-03-24 12:49:04 -0300

    [UPDATE] new design for dash and pallette colors

--- src/app/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/dashboard/components/sidebar/sidebar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  position = "right";
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/dashboard/components/sidebar/sidebar.module.ts
+++ src/app/dashboard/components/sidebar/sidebar.module.ts
@@ -3,13 +3,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
     declarations: [SidebarComponent],
     imports: [
         CommonModule,
         RouterModule,
-        HttpClientModule
+        HttpClientModule,
+        MatIconModule,
+        MatTooltipModule
     ],
     exports: [SidebarComponent],
     providers: [],
